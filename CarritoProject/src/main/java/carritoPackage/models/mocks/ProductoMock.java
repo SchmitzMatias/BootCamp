2f2b5d4bafb7a5e5ee8209763e0ce5eda03d66d0
Matias Schmitz <matias.schmitz@gmail.com>
2018-12-07 11:36:16 -0300

    remove unused imports

--- CarritoProject/src/main/java/carritoPackage/models/mocks/ProductoMock.java
+++ CarritoProject/src/main/java/carritoPackage/models/mocks/ProductoMock.java
@@ -1,14 +1,14 @@
 package carritoPackage.models.mocks;
 
-import java.util.HashMap;
+//import java.util.HashMap;
 
 import carritoPackage.models.Producto;
 
 public class ProductoMock {
     private String nombre="pepa";
 	private long id=123;
-	private Double costo=20.2;
-    private HashMap<String, Object> cualidades= new HashMap();
+	//private Double costo=20.2;
+    //private HashMap<String, Object> cualidades= new HashMap();
     
     public Producto getProductoMock(){
         Producto producto= new Producto();
